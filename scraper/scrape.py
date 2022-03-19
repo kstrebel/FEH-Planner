118e41943abb95aab83e434e7cd574c4628aa303
Kathy Strebel <kthstrebel4@gmail.com>
2022-03-18 20:47:02 -0500

    Get unit entries

--- scraper/scrape.py
+++ scraper/scrape.py
@@ -1,3 +1,4 @@
+from distutils.filelist import findall
 from logging import exception
 from plistlib import UID
 from bs4 import BeautifulSoup
@@ -93,16 +94,21 @@ def main():
     except Exception as ex:
         print(ex)
     else:
-        soup = BeautifulSoup(page, "html.parser")
+        # soup = BeautifulSoup(page.text, "html.parser")
+        soup = BeautifulSoup(pagex, "html.parser")
         unitInfo = soup.find(class_="wikitable hero-infobox")
 
         u_id = dataAfterTh(unitInfo, "Internal ID")
-        u_id = u_id[u_id.find("(") + 1 : u_id.find(")")]
-        u_name = unitInfo.find(style="font-size:160%;").get_text()
-        u_epithet = unitInfo.find(style="font-size:90%").get_text()
-        u_imageAddress = ""
-        u_entry1 = ""
-        u_entry2 = ""
+        u_id = u_id[u_id.find("(") + 1 : u_id.find(")")] or ""
+
+        u_name = unitInfo.find(style="font-size:160%;").get_text() or ""
+        u_epithet = unitInfo.find(style="font-size:90%").get_text() or ""
+        u_imageAddress = "" #ignoring this for now
+
+        entries=getEntries(unitInfo)
+        u_entry1 = entries[0].get_text() or ""
+        u_entry2 = entries[1].get_text() or ""
+        
         u_movement = ""
         u_weapon = ""
         u_special = ""
@@ -133,9 +139,36 @@ def main():
     return 0
 
 
+def getEntries(table):
+    element=table.find("span", string=re.compile("Entry")).parent.find_next_sibling("td")
+
+    entries=element.find_all("i")
+
+    if len(entries)<2:
+        entries=[entries[0], BeautifulSoup("<a></a>", "html.parser")]
+
+    # print()
+    # print()
+    # print()
+    # #print(entries)
+    # print(entries[0].get_text())
+    # print(entries[1].get_text())
+    
+    return entries
+
 def dataAfterTh(table, header):
-    element = table.find("th", string=re.compile(header)).find_next_sibling("td")
-    return element.getText("|", strip=True)
+    element = table.find("th", string=re.compile(header))
+
+    if element==None:
+        element=table.find("span", string=re.compile(header)).parent.find_next_sibling("td")
+    else:
+        element=element.find_next_sibling("td")
+
+    # print()
+    # print()
+    # print()
+    # print(element.get_text("|", strip=True))
+    return element.get_text("|", strip=True)
 
 
 if __name__ == "__main__":
